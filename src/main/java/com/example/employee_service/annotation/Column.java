1a6fe31858c8c6245de68334e3699542874897a8
mạnh giang <manh2362000@gmail.com>
2022-06-10 09:16:46 +0700

    Initial commit

---
+++ src/main/java/com/example/employee_service/annotation/Column.java
@@ -0,0 +1,7 @@
+package com.example.employee_service.annotation;
+
+public @interface Column {
+    String name();
+
+    String type();
+}
---
+++ src/main/java/com/example/employee_service/annotation/Id.java
@@ -0,0 +1,5 @@
+package com.example.employee_service.annotation;
+
+public @interface Id {
+    boolean autoIncrement();
+}
